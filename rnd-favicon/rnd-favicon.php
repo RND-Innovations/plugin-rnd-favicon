d577b637338180f7261030876592806a8e8b3eac
DeveloperDilantha <74175124+DeveloperDilantha@users.noreply.github.com>
2023-04-25 12:54:52 +0700

    1.2.0
    Added browser config facility

--- rnd-favicon/rnd-favicon.php
+++ rnd-favicon/rnd-favicon.php
@@ -5,16 +5,16 @@
  *
  * @package           RNDCorePlugins
  * @author            Developer Dilantha
- * @copyright         2021 RND Innovations
+ * @copyright         2021-2023 RND Innovations
  * @license           GPL-2.0-or-later
  *
  * @rnd-plugin
  * Plugin Name:       RND Favicon
  * Plugin URI:        https://rnd.rodee.ca/core-plugins
  * Description:       This plugin will add a favicon and other important visual icons to the head.
- * Version:           1.0.1
+ * Version:           1.2.0
  * Requires at least: 1.0.0
- * Requires PHP:      5.2
+ * Requires PHP:      8.1
  * Author:            Dilantha
  * Author URI:        https://www.dilantha.org
  * Text Domain:       dilantha
@@ -25,6 +25,11 @@
 // Generate Favicon from : https://www.favicon-generator.org/
 // Learn More: https://evilmartians.com/chronicles/how-to-favicon-in-2021-six-files-that-fit-most-needs
 // Learn More: https://sympli.io/blog/heres-everything-you-need-to-know-about-favicons-in-2020/
+
+
+define( "RND_FAVICON_THEME_COLOR", "#D9221C" );
+define( "RND_FAVICON_BACKG_COLOR", "#ffffff" );
+
 
 function rnd_favicon_add_image($items) {
 
@@ -46,9 +51,52 @@ function rnd_favicon_add_image($items) {
     $str .= '<link rel="apple-touch-icon" href="'.$path.'/apple-icon-152x152.png" sizes="152x152" >';
     $str .= '<link rel="apple-touch-icon" href="'.$path.'/apple-icon-180x180.png" sizes="180x180" >';
     
-    //$str = '<link rel="manifest" href="'.$path.'/manifest.json" type="image/png" >';
+    // Manifest File for Android/Chrome
+    $str .= '<link rel="manifest" href="'.APP_URL.'/manifest.json" >';
     
+    $str .= '<meta name="msapplication-config" content="'.APP_URL.'/browserconfig.xml" />';
+    
+    $str .= '<meta name="theme-color" content="'.RND_FAVICON_THEME_COLOR.'"/>';
     
 	return $items.$str;
 }
 add_filter('page_head', 'rnd_favicon_add_image');
+
+
+    // Generate browserconfig.xml for IE/EDGE
+    if( isset($amp[0]) && $amp[0] == "browserconfig.xml" ){
+        header("Content-Type: text/xml");
+
+        $xml_path  = __DIR__ . '/' . $amp[0];
+        $xml       = get_file_contents_from_a_file( $xml_path );
+        $path      = APP_TMPL_URL . '/plugins/' . basename( dirname( __FILE__ ) ) . '/logos';
+        
+        $xml       = str_replace("{{ICON-PATH}}", $path , $xml);
+        $xml       = str_replace("{{BACKG-COLOR}}", RND_FAVICON_BACKG_COLOR , $xml);        
+
+        echo '<?xml version="1.0" encoding="utf-8"?>';
+        echo $xml; exit;
+    }
+
+
+    // Generate Web Manifest file
+    if( isset($amp[0]) && $amp[0] == "manifest.json" ){
+        header("Content-Type: application/manifest+json");
+
+        $json_path  = __DIR__ . '/' . $amp[0];
+        $json       = get_file_contents_from_a_file( $json_path );
+        
+        $json       = str_replace("{{SITE-NAME}}", SITE_NAME , $json);
+        $json       = str_replace("{{SITE-SHORT}}", SITE_NAME , $json);
+        $json       = str_replace("{{SITE-DESCRIPTION}}", SITE_TAG , $json);
+        $json       = str_replace("{{SITE-URL}}", APP_URL , $json);
+        $json       = str_replace("{{SITE-SCOPE}}", "/" , $json);
+       
+        $json       = str_replace("{{BACKG-COLOR}}", RND_FAVICON_BACKG_COLOR , $json);
+        $json       = str_replace("{{THEME-COLOR}}", RND_FAVICON_THEME_COLOR , $json);
+        
+        $path       = APP_TMPL_URL . '/plugins/' . basename( dirname( __FILE__ ) ) . '/logos';
+        $json       = str_replace("{{ICON-PATH}}", $path , $json);
+        
+        echo $json; exit;
+    }
